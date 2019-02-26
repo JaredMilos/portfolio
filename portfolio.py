0412400dd0c1f3814c215cb5f8fd21dceb42c004
Jared Milos <40134720+JaredMilos@users.noreply.github.com>
2019-02-26 11:45:35 -0800

    Update portfolio.py
    Added routes

--- portfolio.py
+++ portfolio.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/mathapp')
+def mathapp():
+    return redirect('http://13.58.61.159/')
+
+@app.route('/collabolab')
+def collabolab():
+    return redirect('http://18.222.110.209/')
+
 if __name__=="__main__":
     app.run(debug=True) 
